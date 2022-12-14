9dcb117b954e20cb6d25045d0e3f0d83c3e4263f
yolandaayuso <114437997+yolandaayuso@users.noreply.github.com>
2022-12-14 21:50:20 +0100

    Añadidas clases

---
+++ HITO2_IPO_NUEVO/Oferta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HITO2_IPO_NUEVO
+{
+    internal class Oferta
+    {
+        public int Id { get; set; }
+        public Ruta Ruta { get; set; }
+        public string Descripcion { get; set; }
+        public Uri IMG_OFERTA { get; set; }
+
+        public Oferta(int id, Ruta ruta, string descripcion)
+        {
+            Id = id;
+            Descripcion = descripcion;
+            Ruta = ruta;
+        }
+    }
+}
