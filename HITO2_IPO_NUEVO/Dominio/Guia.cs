6e3994ff643a563e42637ff205b8614b881d071a
EnriqueAP6 <Enrique.Albalate@alu.uclm.es>
2022-12-15 18:50:42 +0100

    AÑADIDA LECTURA DE TODOS LOS DATOS

--- HITO2_IPO_NUEVO/Dominio/Guia.cs
+++ HITO2_IPO_NUEVO/Dominio/Guia.cs
@@ -9,12 +9,14 @@ namespace HITO2_IPO_NUEVO
     internal class Guia : Usuario
     {
         public string Disponibilidad { set; get; }
+        public string Idiomas { set; get; }
         public List<Ruta> RutasRealizadas { set; get; }
         public List<Ruta> RutasFuturas { set; get; }
 
-        public Guia(string user, string pass, string name, string lastName, string email, DateTime lastLogin, string telefono, string disponibilidad, List<Ruta> rutasrealizadas, List<Ruta> rutasfuturas) : base(user, pass, name, lastName, email, lastLogin, telefono)
+        public Guia(string user, string pass, string name, string lastName, string email, DateTime lastLogin, string telefono,string idiomas, string disponibilidad, List<Ruta> rutasrealizadas, List<Ruta> rutasfuturas) : base(user, pass, name, lastName, email, lastLogin, telefono)
         {
             Disponibilidad = disponibilidad; 
+            Idiomas = idiomas;
             RutasRealizadas = rutasrealizadas;
             RutasFuturas = rutasfuturas;
         }
